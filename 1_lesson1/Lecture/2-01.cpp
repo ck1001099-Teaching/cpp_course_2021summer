ee6ff246730914b28da60f17d5ce87754d2529c1
Yi Chen <ck1001099@gmail.com>
2021-08-03 21:14:30 +0800

    上傳 lesson1 上課資料

---
+++ 1_lesson1/Lecture/2-01.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "紅葉晚蕭蕭，長亭酒一瓢。";
+	return 0;
+}
---
+++ 1_lesson1/Lecture/2-03.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "紅葉晚蕭蕭，長亭酒一瓢。";
+	cout << "殘雲歸太華，疏雨過中條。";
+	cout << "樹色隨關迥，河聲入海遙。";
+	cout << "帝鄉明日到，猶自夢漁礁。";
+	return 0;
+}
